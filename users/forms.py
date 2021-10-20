abe9ed2912a176926f58a5a3f4ee4981a52f837e
c0053483 <Undercover0659802405>
2021-10-20 16:52:30 +0100

    fix Input Validation

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,54 @@
+import re
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import Required, Email, Length, EqualTo, ValidationError
+
+
+def NameCharacter_check(form, field):
+    excluded_chars = "*?!'^+%&/()=}][{$#@<>"
+    for char in field.data:
+        if char in excluded_chars:
+            raise ValidationError(
+                f"Character {char} is not allowed.")
 
 
 class RegisterForm(FlaskForm):
-    email = StringField()
-    firstname = StringField()
-    lastname = StringField()
-    phone = StringField()
-    password = PasswordField()
-    confirm_password = PasswordField()
-    pin_key = StringField()
+    email = StringField(validators=[Required(), Email()])
+    firstname = StringField(validators=[Required(), NameCharacter_check])
+    lastname = StringField(validators=[Required(), NameCharacter_check])
+    phone = StringField(validators=[Required()])
+    password = PasswordField(validators=[Required(), Length(min=6, max=12,
+                                                            message='Password must be between 6 and 12 characters in length.')])
+    confirm_password = PasswordField(
+        validators=[Required(), EqualTo('password', message='Both password fields must be equal!')])
+    pin_key = StringField(validators=[Required(), Length(min=32, max=32,
+                                                            message='PIN Key must be 32 characters in length.')])
+
     submit = SubmitField()
+
+    def register(self):
+        form = RegisterForm()
+        if form.validate_on_submit():
+            return login()
+
+    def validate_password(self, password):
+        p = re.compile(r'(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[#$%&*+,./:;<>()=?@\[\]_`|{}~])')
+        if not p.match(self.password.data):
+            raise ValidationError("Password must contain at least 1 digit, 1 lowercase, 1 uppercase letter and 1 "
+                                  "special character from #$%&*+,./:;<>()=?@[]_`|{}~.")
+
+    def validate_phone(self, phone):
+        ph = re.compile(r'\d{4}-\d{3}-\d{4}$')
+        if not ph.match(self.phone.data):
+            raise ValidationError("Phone must be of the form XXXX-XXX-XXXX (including the dashes)")
+
+# All form fields must be filled. DONE
+# Email must be a valid email address. DONE
+# Firstname and Lastname must not contain the following characters:  * ? ! ' ^ + % & / ( ) = } ] [ { $ # @ < > DONE
+# TODO Phone must be of the form XXXX-XXX-XXXX (including the dashes) DONE
+# Password must be between 6 and 12 characters in length. DONE
+# Password must contain at least 1 digit, 1 lowercase, 1 uppercase and 1 special character. DONE
+# Password and Confirm Password must match. DONE
+# PIN Key must be exactly 32 characters in length. DONE
+# Relevant validation error messages must be shown. DONE
+
