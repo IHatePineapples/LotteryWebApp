75fa3f53e97e9f80398f79cce150c954de39be38
Big Pineapple <Undercover0659802405>
2021-10-26 05:08:20 +0100

    Add error 403, 500 error handling and update 404 error handling

--- app.py
+++ app.py
@@ -18,9 +18,16 @@ db = SQLAlchemy(app)
 def index():
     return render_template('index.html')
 
+# ERROR HANDLERS/PAGE VIEWS
 @app.errorhandler(404)
 def not_found(e):
-    return render_template("404.html")
+    return render_template("404.html"), 404
+@app.errorhandler(403)
+def not_found(e):
+    return render_template("403.html"), 403
+@app.errorhandler(500)
+def not_found(e):
+    return render_template("500.html"), 500
 
 
 if __name__ == "__main__":
