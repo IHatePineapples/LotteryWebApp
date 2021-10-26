c119269aef1219a2ebd2d848061025b4a81edb90
Big Pineapple <Undercover0659802405>
2021-10-26 19:12:19 +0100

    Change project default Flask SECRET_KEY

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lottery.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'LongAndRandomSecretKey'
+app.config['SECRET_KEY'] = 'VeryLongAndVeryRandomNotSoSecretKey'
 
 # initialise database
 db = SQLAlchemy(app)
