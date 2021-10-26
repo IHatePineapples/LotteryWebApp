45a5702babd52e68b7857f960f8b529489169e9c
Big Pineapple <Undercover0659802405>
2021-10-26 05:35:04 +0100

    Move JS redirect() to /static

---
+++ static/homeRedirect.js
@@ -0,0 +1,9 @@
+const index = "/" // index/homepage
+let timer = 6; // seconds
+
+function redirect() {
+
+    if (timer <=0) window.location = index; // redirects to index
+    else timer--; setTimeout("redirect()", 1000) // sleeps 1 sec and decrements the timer
+
+}
