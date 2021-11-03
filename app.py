a58718de1694e7a256e59df1166c89f102a52a15
Big Pineapple <Undercover0659802405>
2021-11-03 15:04:56 +0000

    Fix error pages views functions / error handlers

--- app.py
+++ app.py
@@ -41,19 +41,19 @@ def index():
 
 
 # ERROR HANDLERS/PAGE VIEWS
-@app.errorhandler(404)
-def not_found(e):
-    return render_template("404.html"), 404
-
-
 @app.errorhandler(403)
-def not_found(e):
-    return render_template("403.html"), 403
+def page_forbidden(error):
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
-def not_found(e):
-    return render_template("500.html"), 500
+def internal_error(error):
+    return render_template('500.html'), 500
 
 
 if __name__ == "__main__":
