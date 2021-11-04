7e035b29d054fc77be94ff17cda47e54ab9480f3
Big Pineapple <Undercover0659802405>
2021-11-04 01:22:37 +0000

    Update lottery database for final submission

--- static/homeRedirect.js
+++
@@ -1,9 +0,0 @@
-//DEPRECATED
-let timer = 6; // seconds
-
-function redirect() {
-
-    if (timer <=0) window.location = "/"; // redirects to index
-    else timer--; setTimeout("redirect()", 1000) // sleeps 1 sec and decrements the timer
-
-}
